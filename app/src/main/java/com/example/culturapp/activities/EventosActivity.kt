a8f8be5d0bfc244b1fd856c684735f6a0d4ad173
JoelCamps <jcampsv2324@politecnics.barcelona>
2025-04-23 16:48:21 +0200

    EventosActivity, usando fragment_eventos.xml

--- app/src/main/java/com/example/culturapp/activities/EventosActivity.kt
+++ app/src/main/java/com/example/culturapp/activities/EventosActivity.kt
@@ -17,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.culturapp.R
 import com.example.culturapp.adapters.EventoAdapter
 import com.example.culturapp.clases.Evento
+import com.example.culturapp.fragments.EventoFragment
 import java.sql.Timestamp
 
 class EventosActivity : AppCompatActivity() {
@@ -27,14 +28,12 @@ class EventosActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        val btnCrear: AppCompatButton = findViewById(R.id.btnCrear)
         val lblTitulo: TextView = findViewById(R.id.lblTitulo)
         val imgEvento: ImageView = findViewById(R.id.imgEvento)
         val lblEvento: TextView = findViewById(R.id.lblEvento)
         val chat: LinearLayout = findViewById(R.id.chat)
         val reserva: LinearLayout = findViewById(R.id.reserva)
         val ajustes: LinearLayout = findViewById(R.id.ajustes)
-        val rvEvento: RecyclerView = findViewById(R.id.RVEvento)
         val seleccionado = ContextCompat.getColor(this, R.color.morado)
 
         lblTitulo.text = getString(R.string.eventos)
@@ -42,20 +41,10 @@ class EventosActivity : AppCompatActivity() {
         lblEvento.setTextColor(seleccionado)
 
         val usuario = intent.getSerializableExtra("userlogin") as? Usuario
-        if (usuario?.tipo?.value == Usuario.Tipo.ORGANIZADOR.value) {
-            btnCrear.visibility = VISIBLE
+        val fragment = usuario?.let { EventoFragment.newInstance(it) }
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
         }
-
-        val listaEventos = listOf(
-            Evento(1, "Charla Motivacional", Timestamp.valueOf("2025-12-22 00:00:00.000"), 100, "Una charla inspiradora para motivar a los asistentes.", 50, Timestamp.valueOf("2025-12-22 00:00:00.000"), true, 1, 1),
-            Evento(2, "Concierto en Vivo", Timestamp.valueOf("2007-07-01 00:00:00.000"), 500, "Un concierto en vivo de música popular.", 100, Timestamp.valueOf("2007-07-01 00:00:00.000"), true, 2, 2),
-            Evento(3, "Partido de Fútbol", Timestamp.valueOf("2021-09-17 00:00:00.000"), 300, "Un emocionante partido de fútbol entre dos grandes equipos.", 40, Timestamp.valueOf("2021-09-17 00:00:00.000"), true, 3, 3),
-            Evento(4, "Torneo de eSports", Timestamp.valueOf("2033-02-13 00:00:00.000"), 200, "Un torneo de eSports donde los mejores jugadores competirán.", 30, Timestamp.valueOf("2033-02-13 00:00:00.000"), true, 4, 4)
-                                 )
-
-        rvEvento.layoutManager = LinearLayoutManager(this)
-        rvEvento.adapter = EventoAdapter(listaEventos)
-        //neceito la api
 
         chat.setOnClickListener{
             val intent = Intent(this, ChatActivity::class.java)
---
+++ app/src/main/java/com/example/culturapp/fragments/EventoFragment.kt
@@ -0,0 +1,66 @@
+package com.example.culturapp.fragments
+
+import Usuario
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.*
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.culturapp.R
+import com.example.culturapp.adapters.EventoAdapter
+import com.example.culturapp.clases.Evento
+import java.sql.Timestamp
+
+class EventoFragment : Fragment() {
+
+    private lateinit var usuario: Usuario
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            usuario = it.getSerializable("userlogin") as Usuario
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+                             ): View? {
+        return inflater.inflate(R.layout.fragment_eventos, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val btnCrear = view.findViewById<Button>(R.id.btnCrear)
+        val rvEvento = view.findViewById<RecyclerView>(R.id.RVEvento)
+
+        if (usuario.tipo.value == Usuario.Tipo.ORGANIZADOR.value) {
+            btnCrear.visibility = VISIBLE
+        } else {
+            btnCrear.visibility = GONE
+        }
+
+        val listaEventos = listOf(
+            Evento(1, "Charla Motivacional", Timestamp.valueOf("2025-12-22 00:00:00.000"), 100, "Una charla inspiradora para motivar a los asistentes.", 50, Timestamp.valueOf("2025-12-22 00:00:00.000"), true, 1, 1),
+            Evento(2, "Concierto en Vivo", Timestamp.valueOf("2007-07-01 00:00:00.000"), 500, "Un concierto en vivo de música popular.", 100, Timestamp.valueOf("2007-07-01 00:00:00.000"), true, 2, 2),
+            Evento(3, "Partido de Fútbol", Timestamp.valueOf("2021-09-17 00:00:00.000"), 300, "Un emocionante partido de fútbol entre dos grandes equipos.", 40, Timestamp.valueOf("2021-09-17 00:00:00.000"), true, 3, 3),
+            Evento(4, "Torneo de eSports", Timestamp.valueOf("2033-02-13 00:00:00.000"), 200, "Un torneo de eSports donde los mejores jugadores competirán.", 30, Timestamp.valueOf("2033-02-13 00:00:00.000"), true, 4, 4)
+                                 )
+
+        rvEvento.layoutManager = LinearLayoutManager(requireContext())
+        rvEvento.adapter = EventoAdapter(listaEventos)
+    }
+
+    companion object {
+        fun newInstance(usuario: Usuario): EventoFragment {
+            val fragment = EventoFragment()
+            val args = Bundle()
+            args.putSerializable("userlogin", usuario)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
